ada3e5119af247b23b2275852f5b9a25263dc02b
guojinfa10837 <guojinfa1@163.com>
2019-07-24 17:26:19 +0800

    ad

--- src/js/addConfig.js
+++ src/js/addConfig.js
@@ -37,6 +37,14 @@ class Fn {
                     }
                  }
                  return arr;
+             },
+             formAmzai(form){
+                let $input = form.find("textarea[name],input[name]");
+                var Obj = {};
+                for(var i=0;i<$input.length;i++){
+                     Obj[$input.eq(i).attr("name")] = $input.eq(i).val();
+                }
+                return Obj;
              }
 
         };
@@ -48,20 +56,42 @@ class Fn {
         let _this = this;
         let opts = this.option;
         let methods = this.methods;
-        console.log('the init page start');
+//console.log('the init page start');
          //读取页面中的dom节点
         this.wrap.jqObj = opts.target;
         this.wrap.configWrap = this.wrap.jqObj.find(".contentLeft");
         this.wrap.liziWrap = this.wrap.jqObj.find(".contentRight");
         this.wrap.messageTip = this.wrap.jqObj.find(".messageTip");
         this.wrap.mask = this.wrap.jqObj.find(".mask");
-
+        let close=()=>{
+           this.wrap.messageTip.hide();
+           this.wrap.mask.hide();
+        };
         this.wrap.messageTip.on("click",".configtab li",function(){
             let type = $(this).attr("type");
             souceData.addType = type;
+        });
+
+        $(document).on("click",".ok",function(){
+              let $form = _this.wrap.messageTip.find(".addListform");
+              let object = methods.formAmzai($form);
+              object.id = new Date().getTime();
+              object.type = Number(_this.wrap.messageTip.find("li.action").attr("type"));
+              object.isSelected = false;
+              if(object.list){
+                  object.list = object.list.replace(/\n/g,",").split(",");
+              }
+              souceData.configQueue.push(object);
+              souceData.configQueue = souceData.configQueue.map(function(currentValue){
+                  return currentValue;
+              });
+              close();
         })
         
-
+        $(document).on("click",".cel",function(){
+           
+            close();
+        })
     };
     
 };
